234136544078b8732bfc30ff6a8658698eacf435
Yannik Schröder <5794325+yschroe@users.noreply.github.com>
2024-08-02 14:42:19 +0200

    add crashing file

---
+++ server/api/test.ts
@@ -0,0 +1,27 @@
+class Project {
+    constructor(
+        public id: number,
+        public projectName: string,
+        public address: string,
+        public type: string,
+        public contactName: string,
+        public contactEmail: string,
+    ) {}
+}
+
+export default defineEventHandler(async (_) => {
+    await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+    });
+
+    return [
+        new Project(
+            1,
+            'Testprojekt 1',
+            'Teststraße 1, 12345 Testhausen',
+            'Testtype',
+            'No Name',
+            'no.name@test.com',
+        ),
+    ];
+});
